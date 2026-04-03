7762b2725e167e033aadf42345483bdf358cb75e
Antoine Duchêne <antoine.duchene@imio.be>
2026-04-03 15:28:41 +0200

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='imio.omnia.core',
-    version='1.0a2',
+    version='1.0a3.dev0',
     description="Shared infrastructure for iMio's Omnia AI suite for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
